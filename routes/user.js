4fa05743d6a241b63ab89cb0dd17848e5823026d
Ansh <anshtandonat@gmail.com>
2022-05-09 15:27:52 +0530

    resolved error

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const userAuth = require("../middlewares/userAuth")
 const router = express.Router()
 
 router.post('/signup',[
-    check("name","Name length must be greater than 3 characters").isLength({min: 3}),
+    check("userName","User Name length must be greater than 3 characters").isLength({min: 3}),
     check("email","Email should be valid").isEmail(),
     check("password","Password must be greater that 6 characters").isLength({min: 6})
 ], signup)

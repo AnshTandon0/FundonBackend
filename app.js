196c4a0087e14de010c4576b5681534db1fd8e71
Ansh <anshtandonat@gmail.com>
2022-05-09 13:11:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+
+
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require("dotenv").config();
+
+//DB
+mongoose.connect(process.env.DATABASE,{
+}).then(()=>{
+    console.log("DB Connected")
+}).catch((err)=>{
+    console.log("Unable to connect to DB "  + err)
+})
+
+//Use parsing middleware
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+//Importing routes
+const userRoutes = require("./routes/user")
+// const adminRoutes = require("./routes/admin")
+// const examRoutes = require("./routes/exam")
+// const questionRoutes = require( "./routes/question" )
+
+//Using routes
+app.use('/api/auth', userRoutes)
+// app.use('/api/admin', adminRoutes)
+// app.use('/api/exam' , examRoutes )
+// app.use('/api/question' , questionRoutes)
+
+
+const port = process.env.PORT || 8000
+
+//Starting a server
+app.listen(port,()=>{
+    console.log(`App is running at ${port}`)
+})
+

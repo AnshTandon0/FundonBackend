e13fe19c15d63280d9402089f363871369aefb3f
Ansh <anshtandonat@gmail.com>
2022-05-10 13:55:58 +0530

    modified cibil in loan apis

--- controllers/loan.js
+++ controllers/loan.js
@@ -7,38 +7,38 @@ const loan = require("../models/loan");
 require('express-jwt');
 
 const create = (req, res) => {
-    const { id, borrowerEmail ,borrowerUserName } = req.body
+    const { id, borrowerEmail, borrowerUserName } = req.body
 
-    const userName = borrowerUserName 
+    const userName = borrowerUserName
     const userEmail = borrowerEmail
 
-    Cibil.findOne({userName , userEmail} , ( err , cibil ) => {
-        if ( cibil )
-        {
-            Cibil.updateOne( { userName , userEmail } ,  {
+    Cibil.findOne({ userName, userEmail }, (err, cibil) => {
+        if (cibil) {
+            Cibil.updateOne({ userName, userEmail }, {
                 $set: {
-                    "loanCount": cibil.loanCount + 1 ,
+                    "loanCount": cibil.loanCount + 1,
                     "disapprovedLoanCount": cibil.disapprovedLoanCount + 1
-                } } , ( err , response ) => {
-                    if ( err )
-                    {
-                        console.log("Error found")
-                    }
-                })
+                }
+            }, (err, response) => {
+                if (err) {
+                    console.log("Error found")
+                }
+            })
         }
-        
-        if ( err || !cibil )
-        {
-            const cibil = new Cibil( { userName : userName , userEmail : userEmail , loanCount : 1 , currentLoanCount : 0 ,
-                finishedOverdue : 0 , securedLoanCount : 0 ,  unsecuredLoanCount : 0 , 
-                loanCountYear : 0 , disapprovedLoanCount : 1 , currentOverdue : 0 , presentLoanAmount : 0 ,
-                amountPaid : 0 , totalLoanCredited : 0 } )
-            cibil.save( (e , cibil) => {
-                if (e )
-                console.log ( e )
+
+        if (err || !cibil) {
+            const cibil = new Cibil({
+                userName: userName, userEmail: userEmail, loanCount: 1, currentLoanCount: 0,
+                finishedOverdue: 0, securedLoanCount: 0, unsecuredLoanCount: 0,
+                loanCountYear: 0, disapprovedLoanCount: 1, currentOverdue: 0, presentLoanAmount: 0,
+                amountPaid: 0, totalLoanCredited: 0
+            })
+            cibil.save((e, cibil) => {
+                if (e)
+                    console.log(e)
                 else
-                console.log( "Cibil Added" )
-            } )
+                    console.log("Cibil Added")
+            })
         }
     })
 
@@ -56,7 +56,7 @@ const create = (req, res) => {
             loan.save((e, loan) => {
                 if (e) {
                     return res.status(400).json({
-                        error: "Loan Already exits ." + e ,
+                        error: "Loan Already exits ." + e,
                     })
                 }
 
@@ -142,7 +142,7 @@ const modify = (req, res) => {
                 })
             }
         })
-    }else{
+    } else {
         res.status(200).json({
             message: "Accepted Loans can not be modified"
         })
@@ -152,42 +152,39 @@ const modify = (req, res) => {
 
 const accept = (req, res) => {
 
-    const { id, lenderUserName, lenderEmail, status, borrowerEmail, borrowerUserName , date , secured , loanAmount } = req.body
+    const { id, lenderUserName, lenderEmail, status, borrowerEmail, borrowerUserName, date, secured, loanAmount } = req.body
 
-    var userName = borrowerUserName 
+    var userName = borrowerUserName
     var userEmail = borrowerEmail
 
-    Cibil.findOne({userName , userEmail} , ( err , cibil ) => {
-        if ( cibil )
-        {
-            var securedLoanCount = cibil.securedLoanCount 
-            var unsecuredLoanCount = cibil.unsecuredLoanCount 
-            if ( secured )
-            {
+    Cibil.findOne({ userName, userEmail }, (err, cibil) => {
+        if (cibil) {
+            var securedLoanCount = cibil.securedLoanCount
+            var unsecuredLoanCount = cibil.unsecuredLoanCount
+            if (secured) {
                 securedLoanCount++
             }
-            else{
+            else {
                 unsecuredLoanCount++
             }
 
-            Cibil.updateOne( { userName , userEmail } ,  {
+            Cibil.updateOne({ userName, userEmail }, {
                 $set: {
-                    "currentLoanCount": cibil.currentLoanCount + 1 ,
-                    "securedLoanCount": securedLoanCount ,
-                    "unsecuredLoanCount": unsecuredLoanCount ,
-                    "loanCountYear": cibil.loanCountYear + 1 ,
-                    "disapprovedLoanCount" : cibil.disapprovedLoanCount - 1 ,
-                    "presentLoanAmount" : cibil.presentLoanAmount + loanAmount ,
-                    "totalLoanCredited" : cibil.totalLoanCredited + loanAmount
-                } } , ( err , response ) => {
-                    if ( err )
-                    {
-                        console.log("Error found ." + err)
-                    }else
-                    {
-                        console.log("Cibil updated")
-                    }
-                })
+                    "currentLoanCount": cibil.currentLoanCount + 1,
+                    "securedLoanCount": securedLoanCount,
+                    "unsecuredLoanCount": unsecuredLoanCount,
+                    "loanCountYear": cibil.loanCountYear + 1,
+                    "disapprovedLoanCount": cibil.disapprovedLoanCount - 1,
+                    "presentLoanAmount": cibil.presentLoanAmount + loanAmount,
+                    "totalLoanCredited": cibil.totalLoanCredited + loanAmount
+                }
+            }, (err, response) => {
+                if (err) {
+                    console.log("Error found ." + err)
+                } else {
+                    console.log("Cibil updated")
+                }
+            })
         }
     })
 
@@ -197,12 +194,12 @@ const accept = (req, res) => {
                 "lenderUserName": lenderUserName,
                 "lenderEmail": lenderEmail,
                 "status": status,
-                "date" : date ,
-                "secured" : secured
+                "date": date,
+                "secured": secured
             }
         }, (err, response) => {
             if (response) {
-    
+
                 const transporter = nodemailer.createTransport({
                     service: 'gmail',
                     auth: {
@@ -216,7 +213,7 @@ const accept = (req, res) => {
                     subject: 'Your loan got accepted',
                     text: `Your loan with id ${id} got accepted by ${lenderUserName}. You can view further details on the app .`
                 };
-    
+
                 transporter.sendMail(mailOptions, (error, info) => {
                     if (error) {
                         res.json({
@@ -224,14 +221,14 @@ const accept = (req, res) => {
                         })
                     }
                 })
-    
+
                 const mailOptions2 = {
                     from: 'a.antsapps@gmail.com',
                     to: lenderEmail,
                     subject: 'Your accepted the loan',
                     text: `You accepted the loan with id ${id} posted by ${borrowerUserName}. You can view further details on the app .`
                 };
-    
+
                 transporter.sendMail(mailOptions2, (error, info) => {
                     if (error) {
                         res.json({
@@ -239,13 +236,13 @@ const accept = (req, res) => {
                         })
                     }
                 })
-    
+
                 return res.status(200).json({
                     message: "Loan Accepted Successfully",
                     response
                 })
             }
-    
+
             if (err || !response) {
                 res.status(400).json({
                     message: "Such Loan Does Not Exists"
@@ -277,7 +274,7 @@ const all_loans = (req, res) => {
 
     const status = "applied"
 
-    Loan.find({status}, (err, response) => {
+    Loan.find({ status }, (err, response) => {
         if (response[0]) {
             return res.status(200).json({
                 message: "All Loans",
@@ -295,9 +292,9 @@ const all_loans = (req, res) => {
 
 const applied_loans = (req, res) => {
 
-    const { borrowerEmail , borrowerUserName } = req.body
+    const { borrowerEmail, borrowerUserName } = req.body
 
-    Loan.find({borrowerEmail , borrowerUserName}, (err, response) => {
+    Loan.find({ borrowerEmail, borrowerUserName }, (err, response) => {
         if (response[0]) {
             return res.status(200).json({
                 message: "All Loans",
@@ -315,9 +312,9 @@ const applied_loans = (req, res) => {
 
 const accepted_loans = (req, res) => {
 
-    const { lenderEmail , lenderUserName } = req.body
+    const { lenderEmail, lenderUserName } = req.body
 
-    Loan.find({lenderEmail , lenderUserName}, (err, response) => {
+    Loan.find({ lenderEmail, lenderUserName }, (err, response) => {
         if (response[0]) {
             return res.status(200).json({
                 message: "All Loans",
@@ -333,12 +330,142 @@ const accepted_loans = (req, res) => {
     })
 }
 
+const paid_amount = (req, res) => {
+
+    const { userName, userEmail, amountPaid, overdue } = req.body
+
+    Cibil.findOne({ userName, userEmail }, (err, cibil) => {
+        if (cibil) {
+            if (overdue) {
+                Cibil.updateOne({ userName, userEmail }, {
+                    $set: {
+                        "presentLoanAmount": cibil.presentLoanAmount - amountPaid,
+                        "amountPaid": cibil.amountPaid + amountPaid ,
+                        "currentOverdue" : cibil.currentOverdue + 1 
+                    }
+                }, (err, response) => {
+                    if (err) {
+                        console.log("Error found ." + err)
+                    } else {
+                        console.log("Cibil updated")
+                    }
+                })
+            }
+
+            Cibil.updateOne({ userName, userEmail }, {
+                $set: {
+                    "presentLoanAmount": cibil.presentLoanAmount - amountPaid,
+                    "amountPaid": cibil.amountPaid + amountPaid
+                }
+            }, (err, response) => {
+                if (err) {
+                    console.log("Error found ." + err)
+                } else {
+                    console.log("Cibil updated")
+                }
+            })
+        }
+    })
+
+}
+
+const finished_loan = ( req, res ) => {
+    const { id, lenderUserName, lenderEmail, status, borrowerEmail, borrowerUserName, overdue } = req.body
+
+    var userName = borrowerUserName
+    var userEmail = borrowerEmail
+
+    Cibil.findOne({ userName, userEmail }, (err, cibil) => {
+        if (cibil) {
+            
+            var a = 0 
+
+            if (overdue) {
+                a++
+            }
+
+            Cibil.updateOne({ userName, userEmail }, {
+                $set: {
+                    "finishedOverdue": cibil.finishedOverdue + a
+                }
+            }, (err, response) => {
+                if (err) {
+                    console.log("Error found ." + err)
+                } else {
+                    console.log("Cibil updated")
+                }
+            })
+        }
+    })
+
+
+    Loan.updateOne({ id }, {
+        $set: {
+            "status": status,
+        }
+    }, (err, response) => {
+        if (response) {
+
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'a.antsapps@gmail.com',
+                    pass: 'qybdrvmddxnivpqu'
+                }
+            });
+            const mailOptions = {
+                from: 'a.antsapps@gmail.com',
+                to: borrowerEmail,
+                subject: 'Your completed the loan amount',
+                text: `Your loan with id ${id} is completely paid to ${lenderUserName}. You can view further details on the app .`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    res.json({
+                        error: error
+                    })
+                }
+            })
+
+            const mailOptions2 = {
+                from: 'a.antsapps@gmail.com',
+                to: lenderEmail,
+                subject: 'Your got the complete loan amount',
+                text: `You are paid the complete loan amount with id ${id} by ${borrowerUserName}. You can view further details on the app .`
+            };
+
+            transporter.sendMail(mailOptions2, (error, info) => {
+                if (error) {
+                    res.json({
+                        error: error
+                    })
+                }
+            })
+
+            return res.status(200).json({
+                message: "Loan Paid Successfully",
+                response
+            })
+        }
+
+        if (err || !response) {
+            res.status(400).json({
+                message: "Such Loan Does Not Exists"
+            })
+        }
+    })
+
+}
+
 module.exports = {
     "create": create,
     "modify": modify,
     "get": get,
     "accept": accept,
     "all_loans": all_loans,
-    "applied_loans":applied_loans,
-    "accepted_loans":accepted_loans
+    "applied_loans": applied_loans,
+    "accepted_loans": accepted_loans,
+    "paid_amount": paid_amount,
+    "finished_loan": finished_loan
 }
--- routes/loan.js
+++ routes/loan.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {create , modify , get , accept , all_loans , accepted_loans , applied_loans} = require("../controllers/loan");
+const {create , modify , get , accept , all_loans , accepted_loans , applied_loans , paid_amount , finished_loan} = require("../controllers/loan");
 const {check} = require("express-validator")
 const userAuth = require("../middlewares/userAuth");
 const { route } = require("./user");
@@ -7,10 +7,12 @@ const router = express.Router()
 
 router.post('/create', userAuth , create)
 router.post('/modify', userAuth , modify)
+router.post('/accept', userAuth , accept)
+router.get('/get', userAuth , get)
 router.get('/allLoans', userAuth , all_loans)
 router.get('/appliedLoans', userAuth , applied_loans)
 router.get('/acceptedLoans', userAuth , accepted_loans)
-router.get('/get', userAuth , get)
-router.post('/accept', userAuth , accept)
+router.get('/paidAmount', userAuth , paid_amount)
+router.get('/finishedLoans', userAuth , finished_loan)
 
 module.exports = router
